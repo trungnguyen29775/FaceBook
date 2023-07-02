a929095c00161062d29dfe33b7d9d89a64bdbdf7
Nguyễn Trần Minh Trung <91869278+trungnguyen29775@users.noreply.github.com>
2023-07-02 15:49:15 +0700

    2/7/2023: New feed 80% done

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,7 +2,7 @@ import './home.css'
 import React from 'react'
 import { BsSearch,BsMessenger,BsCameraVideoFill,BsPencilSquare,BsFillPeopleFill,BsLink45Deg } from "react-icons/bs";
 import { IoNotifications,IoApps } from "react-icons/io5";
-import {AiOutlineLike,AiOutlineGlobal,AiOutlineLeft,AiOutlineRight, AiOutlinePlus,AiOutlineUserSwitch,AiOutlineDown,AiFillHome,AiFillFlag,AiOutlineNotification } from "react-icons/ai";
+import {AiOutlineComment,AiOutlineLike,AiOutlineGlobal,AiOutlineLeft,AiOutlineRight, AiOutlinePlus,AiOutlineUserSwitch,AiOutlineDown,AiFillHome,AiFillFlag,AiOutlineNotification } from "react-icons/ai";
 import { FiX } from "react-icons/fi";
 import { IoIosMore,IoIosBook } from "react-icons/io";
 import { BiShare,BiExpand,BiMoviePlay } from "react-icons/bi";
@@ -153,6 +153,8 @@ class  Home extends React.Component
     render()
     {
         const userSrcImg = "assets/image/avt-user-login.jpg";
+        const userStatusImg = "assets/image/user-status-image.jpg";
+        
         return(
             <div className='home-container'>
                 <nav className='nav'>
@@ -657,53 +659,72 @@ class  Home extends React.Component
 
                     <div className="new-feed-container">
                         <div className='new-feed-header'>
-                            <div>
+                            <div className='new-feed-header-avt-detail'>
                                 <img src={userSrcImg} className='home-avt__img'/>
-                            <div>
-                                <div>
-                                    <span>Trung Nguyen</span>
-                                    <span>4 Tháng 2, 2022</span>
+                            <div className='new-feed-header-detail'>
+                                    <span className='new-feed-header-detail-name-user'>Trung Nguyen</span>
+                                    <span className='new-feed-header-detail-date'>4 Tháng 2, 2022 <span className='dot-seperate'>.</span><AiOutlineGlobal style={{margin:"auto"}}/></span>
                             </div>
-                                <AiOutlineGlobal/>
                             </div>
-                        
+                            <div className='new-feed-more-icon-container'>
+                                <IoIosMore style={{fontSize:"20px",margin:"auto"}}/>
                             </div>
-                            <IoIosMore/>
                         </div>
-                        <div>
-                            <span>Hello</span>
-                            <img/>
+                        <div className='new-feed-content-container'>
+                            <span className='new-feed-status'>Hello</span>
+                            <img className='new-feed-user-status__img' src={userStatusImg} alt='status image'/>
                         </div>
-                        <div>
-                            <div>
-                                <div>
-                                    <img/>
-                                    <img/>
-                                    <img/>
+                        <div className='new-feed-react-comment-count-container'>
+                            <div className='new-feed-react-container'>
+                                <div className='new-feed-react'>
+                                    <img className='new-feed-reacted__img' src="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 16 16'%3e%3cdefs%3e%3clinearGradient id='a' x1='50%25' x2='50%25' y1='10.25%25' y2='100%25'%3e%3cstop offset='0%25' stop-color='%23FEEA70'/%3e%3cstop offset='100%25' stop-color='%23F69B30'/%3e%3c/linearGradient%3e%3clinearGradient id='d' x1='50%25' x2='50%25' y1='0%25' y2='100%25'%3e%3cstop offset='0%25' stop-color='%23472315'/%3e%3cstop offset='100%25' stop-color='%238B3A0E'/%3e%3c/linearGradient%3e%3clinearGradient id='e' x1='50%25' x2='50%25' y1='0%25' y2='81.902%25'%3e%3cstop offset='0%25' stop-color='%23FC607C'/%3e%3cstop offset='100%25' stop-color='%23D91F3A'/%3e%3c/linearGradient%3e%3cfilter id='c' width='118.8%25' height='118.8%25' x='-9.4%25' y='-9.4%25' filterUnits='objectBoundingBox'%3e%3cfeGaussianBlur in='SourceAlpha' result='shadowBlurInner1' stdDeviation='1'/%3e%3cfeOffset dy='-1' in='shadowBlurInner1' result='shadowOffsetInner1'/%3e%3cfeComposite in='shadowOffsetInner1' in2='SourceAlpha' k2='-1' k3='1' operator='arithmetic' result='shadowInnerInner1'/%3e%3cfeColorMatrix in='shadowInnerInner1' values='0 0 0 0 0.921365489 0 0 0 0 0.460682745 0 0 0 0 0 0 0 0 0.35 0'/%3e%3c/filter%3e%3cpath id='b' d='M16 8A8 8 0 110 8a8 8 0 0116 0'/%3e%3c/defs%3e%3cg fill='none'%3e%3cuse fill='url(%23a)' xlink:href='%23b'/%3e%3cuse fill='black' filter='url(%23c)' xlink:href='%23b'/%3e%3cpath fill='url(%23d)' d='M3 8.008C3 10.023 4.006 14 8 14c3.993 0 5-3.977 5-5.992C13 7.849 11.39 7 8 7c-3.39 0-5 .849-5 1.008'/%3e%3cpath fill='url(%23e)' d='M4.541 12.5c.804.995 1.907 1.5 3.469 1.5 1.563 0 2.655-.505 3.459-1.5-.551-.588-1.599-1.5-3.459-1.5s-2.917.912-3.469 1.5'/%3e%3cpath fill='%232A3755' d='M6.213 4.144c.263.188.502.455.41.788-.071.254-.194.369-.422.371-.78.011-1.708.255-2.506.612-.065.029-.197.088-.332.085-.124-.003-.251-.058-.327-.237-.067-.157-.073-.388.276-.598.545-.33 1.257-.48 1.909-.604a7.077 7.077 0 00-1.315-.768c-.427-.194-.38-.457-.323-.6.127-.317.609-.196 1.078.026a9 9 0 011.552.925zm3.577 0a8.953 8.953 0 011.55-.925c.47-.222.95-.343 1.078-.026.057.143.104.406-.323.6a7.029 7.029 0 00-1.313.768c.65.123 1.363.274 1.907.604.349.21.342.44.276.598-.077.18-.203.234-.327.237-.135.003-.267-.056-.332-.085-.797-.357-1.725-.6-2.504-.612-.228-.002-.351-.117-.422-.37-.091-.333.147-.6.41-.788z'/%3e%3c/g%3e%3c/svg%3e" />
+                                    <img className='new-feed-reacted__img' src="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 16 16'%3e%3cdefs%3e%3clinearGradient id='a' x1='50%25' x2='50%25' y1='0%25' y2='100%25'%3e%3cstop offset='0%25' stop-color='%2318AFFF'/%3e%3cstop offset='100%25' stop-color='%230062DF'/%3e%3c/linearGradient%3e%3cfilter id='c' width='118.8%25' height='118.8%25' x='-9.4%25' y='-9.4%25' filterUnits='objectBoundingBox'%3e%3cfeGaussianBlur in='SourceAlpha' result='shadowBlurInner1' stdDeviation='1'/%3e%3cfeOffset dy='-1' in='shadowBlurInner1' result='shadowOffsetInner1'/%3e%3cfeComposite in='shadowOffsetInner1' in2='SourceAlpha' k2='-1' k3='1' operator='arithmetic' result='shadowInnerInner1'/%3e%3cfeColorMatrix in='shadowInnerInner1' values='0 0 0 0 0 0 0 0 0 0.299356041 0 0 0 0 0.681187726 0 0 0 0.3495684 0'/%3e%3c/filter%3e%3cpath id='b' d='M8 0a8 8 0 00-8 8 8 8 0 1016 0 8 8 0 00-8-8z'/%3e%3c/defs%3e%3cg fill='none'%3e%3cuse fill='url(%23a)' xlink:href='%23b'/%3e%3cuse fill='black' filter='url(%23c)' xlink:href='%23b'/%3e%3cpath fill='white' d='M12.162 7.338c.176.123.338.245.338.674 0 .43-.229.604-.474.725a.73.73 0 01.089.546c-.077.344-.392.611-.672.69.121.194.159.385.015.62-.185.295-.346.407-1.058.407H7.5c-.988 0-1.5-.546-1.5-1V7.665c0-1.23 1.467-2.275 1.467-3.13L7.361 3.47c-.005-.065.008-.224.058-.27.08-.079.301-.2.635-.2.218 0 .363.041.534.123.581.277.732.978.732 1.542 0 .271-.414 1.083-.47 1.364 0 0 .867-.192 1.879-.199 1.061-.006 1.749.19 1.749.842 0 .261-.219.523-.316.666zM3.6 7h.8a.6.6 0 01.6.6v3.8a.6.6 0 01-.6.6h-.8a.6.6 0 01-.6-.6V7.6a.6.6 0 01.6-.6z'/%3e%3c/g%3e%3c/svg%3e"/>
+                                    <img  className='new-feed-reacted__img' src="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 16 16'%3e%3cdefs%3e%3clinearGradient id='a' x1='50%25' x2='50%25' y1='0%25' y2='100%25'%3e%3cstop offset='0%25' stop-color='%23FF6680'/%3e%3cstop offset='100%25' stop-color='%23E61739'/%3e%3c/linearGradient%3e%3cfilter id='c' width='118.8%25' height='118.8%25' x='-9.4%25' y='-9.4%25' filterUnits='objectBoundingBox'%3e%3cfeGaussianBlur in='SourceAlpha' result='shadowBlurInner1' stdDeviation='1'/%3e%3cfeOffset dy='-1' in='shadowBlurInner1' result='shadowOffsetInner1'/%3e%3cfeComposite in='shadowOffsetInner1' in2='SourceAlpha' k2='-1' k3='1' operator='arithmetic' result='shadowInnerInner1'/%3e%3cfeColorMatrix in='shadowInnerInner1' values='0 0 0 0 0.710144928 0 0 0 0 0 0 0 0 0 0.117780134 0 0 0 0.349786932 0'/%3e%3c/filter%3e%3cpath id='b' d='M8 0a8 8 0 100 16A8 8 0 008 0z'/%3e%3c/defs%3e%3cg fill='none'%3e%3cuse fill='url(%23a)' xlink:href='%23b'/%3e%3cuse fill='black' filter='url(%23c)' xlink:href='%23b'/%3e%3cpath fill='white' d='M10.473 4C8.275 4 8 5.824 8 5.824S7.726 4 5.528 4c-2.114 0-2.73 2.222-2.472 3.41C3.736 10.55 8 12.75 8 12.75s4.265-2.2 4.945-5.34c.257-1.188-.36-3.41-2.472-3.41'/%3e%3c/g%3e%3c/svg%3e"/>
                                 </div>
-                                <div>
+                                <div className='new-feed-user-react-container'>
                                     <span>Lan Anh, Trần Tài và 61 người khác</span>
                                 </div>
                             </div>
                             <span>26 bình luận</span>
+                            
                         </div>
                         {/* Post Action */}
-                        <div>
-                            <div>
-                            <AiOutlineLike/>
-                            <span>Thích</span>
+                        <div className='line-through status new-feed'>
+
+
+                        </div>
+
+                        <div className='new-feed-action-container'>
+                            <div className='new-feed-action'>
+                                <div className='new-feed-action--center'>
+                                <AiOutlineLike style={{fontSize:"18px",padding:"6px 4px"}}/>
+                                <span>Thích</span>
+                                </div>
+                           
                             </div>
-                            <div>
-                            <FaRegCommentAlt/>
+                            <div className='new-feed-action'>
+                            <div className='new-feed-action--center'>
+                            <AiOutlineComment style={{fontSize:"18px",padding:"6px 4px"}}/>
                             <span>
                                 Bình luận
                             </span>
                             </div>
-                            <div>
-                                <BiShare/>
+                           
+                            </div>
+                            
+                            <div className='new-feed-action'>
+                                <div className='new-feed-action--center'>
+                                <BiShare style={{fontSize:"18px",padding:"6px 4px"}}/>
                                 <span>Chia sẻ</span>
+                                </div>
+                                
                             </div>
                         </div>
+
+                        <div className='line-through status new-feed'>
+                        </div>
+
+                        
                         {/* Comment */}
                         <div>
                             <div>
