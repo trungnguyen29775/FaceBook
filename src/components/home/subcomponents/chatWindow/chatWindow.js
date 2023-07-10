bbae386cbdf35a6f42087611d87400e18161a64a
Nguyễn Trần Minh Trung <91869278+trungnguyen29775@users.noreply.github.com>
2023-07-10 11:18:25 +0700

    10/7/2023: Done Chat window

--- src/components/home/subcomponents/chatWindow/chatWindow.js
+++ src/components/home/subcomponents/chatWindow/chatWindow.js
@@ -1,67 +1,80 @@
 import './chatWindow.css'
-import { AiOutlineDown } from "react-icons/ai";
+import { AiOutlineGif,AiOutlineDown,AiFillPlusCircle,AiFillLike } from "react-icons/ai";
 import { HiVideoCamera } from "react-icons/hi2";
 import { CgLoadbar } from "react-icons/cg";
 import { FiX } from "react-icons/fi";
-import { FaPhoneAlt } from "react-icons/fa";
+import { FaImages,FaSmile,FaPhoneAlt } from "react-icons/fa";
+import { LuSticker } from "react-icons/lu";
 import { useEffect, useState } from 'react';
 
 const ChatWindow = function()
 {
 
-    const  [data,setData] = useState()
+    const  [data,setData] = useState({
+        userId:null,
+        userSrcImg:"",
+        userName:"",
+        mess:{
+            currentUser:[
+                
+            ],
+            targetUser:[
+                
+            ]
+        }
+    })
     useEffect(()=>
     {
         // call api to get data
-        setData(
-            {
-                userId:1,
-                userSrcImg:"assets/image/avt-user-login.jpg",
-                userName:"Phúc Lê",
-                mess:{
-                    currentUser:[
-                        {
-                            mess:"Hello",
-
-                        },
-                        {
-                            mess:"Hello",
-                            
-                        },
-                        {
-                            mess:"Hello",
-                            
-                        },
-                        {
-                            mess:"Hello",
-                            
-                        }
-                    ],
-                    targetUser:[
-                        {
-                            mess:"Hello",
-                            
-                        },
-                        {
-                            mess:"Hello",
-                            
-                        },
-                        {
-                            mess:"Hello",
-                            
-                        },
-                        {
-                            mess:"Hello",
-                            
-                        }
-                    ]
-                }
+        setData({
+            userId:1,
+            userSrcImg:"assets/image/avt-user-login.jpg",
+            userName:"Phúc Lê",
+            mess:{
+                currentUser:[
+                    {
+                        mess:"Hello",
+    
+                    },
+                    {
+                        mess:"Hello",
+                        
+                    },
+                    {
+                        mess:"Hello",
+                        
+                    },
+                    {
+                        mess:"Hello",
+                        
+                    }
+                ],
+                targetUser:[
+                    {
+                        mess:"Hello",
+                        
+                    },
+                    {
+                        mess:"Hello",
+                        
+                    },
+                    {
+                        mess:"Hello",
+                        
+                    },
+                    {
+                        mess:"Hello",
+                        
+                    }
+                ]
             }
-        )
+        })
+        console.log(data)
     },[])
 
     return(
         <div className='mess-window-container'>
+            {/* Header */}
             <div className='mess-window-header-container'>
                 
                 <div className='mess-window-header-user-detail-container'>
@@ -102,7 +115,80 @@ const ChatWindow = function()
                 </div>
 
             </div>
-            <div className='mess-window-seperate'></div>
+
+
+            {/* Content */}
+            <div className='mess-chat-content-container'>
+                <div className='mess-chat-content-user-backgrounnd-info'>
+                    <div className='mess-chat-content-user-backgrounnd-info-img-container' >
+                        <img alt={data.userName} src={data.userSrcImg} className='mess-chat-content-user-backgrounnd-info__img' />
+                        <div></div>
+                    </div>
+                    <div className='mess-chat-content-user-backgrounnd-info-span-container'>
+                        <span className='mess-chat-content-user-backgrounnd-info-span-container-name'>{data.userName}</span>
+                        <span>Facebook</span>
+                        <span>Các  bạn là bạn bè trên Facebook</span>
+                    </div>
+                </div>
+                <div className='mess-chat-content-other-container'>
+                    <img src={data.userSrcImg} className='mess-chat-content__img'/>
+                    <div className='mess-chat-content-message-container'>
+                        <span>Hello</span>
+                        <span>Hello</span>
+
+                    </div>
+                </div>
+
+                <div className='mess-chat-content-current-container'>
+                    <img src={data.userSrcImg} className='mess-chat-content__img'/>
+                    <div className='mess-chat-content-message-container'>
+                        <span>Hello</span>
+                        <span>Hello</span>
+
+                        
+
+                    </div>
+                </div>
+
+            </div>
+
+
+            {/* Footer */}
+
+            <div className='mess-window-input-container'>
+
+                <div className='mess-window-input-icon-container'>
+                    <AiFillPlusCircle style={{margin:"auto"}}/>
+                    
+                </div>
+                <div className='mess-window-input-textarea-icon-container'>
+                    <div className='mess-window-input-icon-container-wraper'>
+                        <div className='mess-window-input-icon-container'>
+                            <FaImages style={{margin:"auto"}}/>
+
+                        </div>
+                        <div className='mess-window-input-icon-container'>
+                        <LuSticker style={{margin:"auto"}}/>
+
+                        </div>
+                        <div className='mess-window-input-icon-container'>
+                        <AiOutlineGif style={{margin:"auto"}}/>
+
+                        </div>
+                    </div>
+                    <div className='mess-window-input-textarea-container'>
+                        <textarea placeholder="Aa"></textarea>
+                        
+                        <div className='mess-window-input-icon-container center'>
+                            <FaSmile style={{margin:"auto"}}/>    
+                        </div>
+                    </div>
+                </div>
+                <div className='mess-window-input-icon-container padding'>
+                    <AiFillLike style={{margin:"auto"}}/>
+                </div>
+
+            </div>
         </div>
     )
 }
